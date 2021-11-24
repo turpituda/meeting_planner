70221662b6ec9c039248ea24f9bc4fcb078b34f7
turpituda <ljubicavavan96@gmail.com>
2021-11-24 22:18:12 +0100

    creating first pages (home, date, about)

--- meeting_planner/urls.py
+++ meeting_planner/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path
 
-from website.views import welcome
+from website.views import welcome, date, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('welcome.html',welcome),
+    path('',welcome),
+    path('date',date),
+    path('about',about)
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
+
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page is served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("Hi, I'm Ljubi!")
